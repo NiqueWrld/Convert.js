a57dc849e6afd1cd86f86fef4263c6e1a0b1426b
Niquewrld <119890333+NiqueWrld@users.noreply.github.com>
2024-01-17 00:19:55 +0200

    Update conv.js

--- conv.js
+++ conv.js
@@ -1,1 +1,15 @@
 console.log("Thank you for using convert.js by NiqueWrld");
+
+//1. JSON to Object Conversion: Converting JSON (JavaScript Object Notation) data to a JavaScript object.
+
+class jsonToObject {
+  constructor() {}
+  convertToObject(jsonString) {
+    try {
+      return JSON.parse(jsonString);
+    } catch (error) {
+      console.error("Error parsing JSON: ", error);
+      return null;
+    }
+  }
+}
